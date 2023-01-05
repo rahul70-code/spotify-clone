fa46229f75075850b2aa4d773a601af018fbe946
Rahul Talreja <r.talreja@interproductec.com>
2023-01-05 20:31:18 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("welcome to spotify");
+let songs = [
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+
+]
+let audioElement = new Audio('songs/1.mp3')
+// audioElement.play()
